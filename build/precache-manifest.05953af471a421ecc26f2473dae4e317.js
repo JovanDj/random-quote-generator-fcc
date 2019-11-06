4cc5af6b3452907465a0fb1d476366c3a83e01f5
Jovan <jovandjukic2@gmail.com>
2019-11-06 10:03:04 +0100

    Fix styling

---
+++ build/precache-manifest.05953af471a421ecc26f2473dae4e317.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a50b2ecaacd7dd6a8c20acdab0a5d969",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d1904ba04bb334e1e7e",
+    "url": "/static/css/main.8d30b44e.chunk.css"
+  },
+  {
+    "revision": "f865e86419294e3de9f9",
+    "url": "/static/js/2.42d0c590.chunk.js"
+  },
+  {
+    "revision": "1d1904ba04bb334e1e7e",
+    "url": "/static/js/main.e5ff7094.chunk.js"
+  },
+  {
+    "revision": "598c31d22adf4b91cc35",
+    "url": "/static/js/runtime-main.8d630554.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ class App extends Component {
 
     this.state = {
       error: undefined,
-      loading: false,
+      loading: true,
       quote: {
-        author: "Author",
-        text: "Text"
+        author: "",
+        text: ""
       }
     };
   }
@@ -18,34 +18,58 @@ class App extends Component {
     return (
       <div id="quote-box">
         <div className="container">
-          <h1>Random Quote generator</h1>
-
-          {this.state.loading && <p>Please wait..</p>}
           {this.state.error && (
-            <p className="text-danger">{this.state.error}</p>
+            <div className="alert alert-danger" role="alert">
+              {this.state.error}
+            </div>
           )}
+          <blockquote class="blockquote bg-light p-5 text-center m-5">
+            {!this.state.loading && (
+              <React.Fragment>
+                <p className="mb-0 quote-text lead" id="text">
+                  {this.state.quote.text}
+                </p>
+                <footer class="blockquote-footer my-2">
+                  <cite
+                    className="quote-author"
+                    id="author"
+                    title={this.state.quote.author}
+                  >
+                    {this.state.quote.author}
+                  </cite>
+                </footer>
+                <div className="buttons container d-flex justify-content-between mt-5">
+                  <button
+                    className="btn btn-primary"
+                    id="new-quote"
+                    onClick={this.fetchQuote}
+                  >
+                    New Quote
+                  </button>
 
-          <div className="quote-text" id="text">
-            {this.state.quote.text}
-          </div>
-          <div className="quote-author" id="author">
-            {this.state.quote.author}
-          </div>
-          <button id="new-quote" onClick={this.fetchQuote}>
-            New Quote
-          </button>
+                  <a
+                    className="btn btn-primary"
+                    role="button"
+                    href={
+                      "https://twitter.com/intent/tweet?text=" +
+                      this.state.quote.text +
+                      " - " +
+                      this.state.quote.author
+                    }
+                    id="tweet-quote"
+                  >
+                    Share on Twitter
+                  </a>
+                </div>
+              </React.Fragment>
+            )}
 
-          <a
-            href={
-              "https://twitter.com/intent/tweet?text=" +
-              this.state.quote.text +
-              " - " +
-              this.state.quote.author
-            }
-            id="tweet-quote"
-          >
-            Click
-          </a>
+            {this.state.loading && (
+              <div className="spinner-border" role="status">
+                <span className="sr-only">Loading...</span>
+              </div>
+            )}
+          </blockquote>
         </div>
       </div>
     );
@@ -74,10 +98,16 @@ class App extends Component {
             },
             loading: false
           });
-        }
+        },
         // Note: it's important to handle errors here
         // instead of a catch() block so that we don't swallow
         // exceptions from actual bugs in components.
+        error => {
+          this.setState({
+            loading: false,
+            error
+          });
+        }
       );
   };
 }
